865c2f7acc479b904c1afe47bec7be7e88cfc68d
Kevin <jimenez.kevin0601@gmail.com>
2025-11-10 23:28:01 -0600

    Implement backend logic, ignore node_modules and .env

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-async function getWeather() {
-    const city = document.getElementById("cityInput").value;
-    const apiKey = 'c4ff24c82c491fdb0173fec6a4dbe804';
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-   
- try{
-    const response = await fetch(url);
+async function getWeather(city){
+    const response = await fetch(`http://localhost:3000/weather?city=${city}`);
     const data = await response.json();
 
-    if(data.cod == "404"){
-        document.getElementById("weatherResult").innerHTML = "City not found";
-    }else{
-        document.getElementById("weatherResult").innerHTML = 
-        `<h2>${data.name}, ${data.sys.country}</h2>
-        <p>Temperature: ${data.main.temp} °C</p>
-        <p>Weather: ${data.weather[0].main}</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-        `;
-    }
-}   catch (error){
-    document.getElementById("weatherResult").innerHTML = "Error fetching data";
-    console.error( "Fetch error: ", error);
-    console.log("API response: ", data);
- }
+    console.log(data);
+    document.querySelector('#result').textContent = 
+    data.main ? `${data.name}: ${data.main.temp}°C` : data.error || 'City not found';
 }
+
+document.querySelector('#btn').addEventListener('click', () => {
+    const city = document.querySelector('#city').value;
+    getWeather(city);
+});
